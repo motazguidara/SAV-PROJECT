5eb8e8e8d7067a56d7986807ecb2e26022bd81e7
motazguidara <92393869+motazguidara@users.noreply.github.com>
2026-01-08 11:58:37 +0100

    Enable response compression for Blazor assets

--- src/Frontend/Server/Program.cs
+++ src/Frontend/Server/Program.cs
@@ -1,10 +1,28 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddResponseCompression(options =>
+{
+    options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[]
+    {
+        "application/octet-stream",
+        "application/wasm"
+    });
+});
 
 var app = builder.Build();
+app.UseResponseCompression();
 app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
+var contentTypeProvider = new FileExtensionContentTypeProvider();
+contentTypeProvider.Mappings[".wasm"] = "application/wasm";
+contentTypeProvider.Mappings[".dll"] = "application/octet-stream";
+app.UseStaticFiles(new StaticFileOptions
+{
+    ContentTypeProvider = contentTypeProvider
+});
 
 app.MapFallbackToFile("index.html");
 
